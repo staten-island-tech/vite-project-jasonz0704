73c1afb966c4ae5ce96915ad7925abfa53150e67
jasonz0704 <oonetwome@gmail.com>
2025-12-01 14:00:55 -0500

    6

--- app/src/main.js
+++ app/src/main.js
@@ -3,14 +3,3 @@ import "./style.css";
 document.getElementById("memeform").addEventListener("submit", function (e) {
   e.preventDefault();
 });
-
-/* function draw() {
-  const memecanvas = document.getElementById("memecanvas");
-  const ctx = memecanvas.getContext("2d");
-
-  ctx.insertAdjacentHTML("afterbegin",
-    `<div class="toptext" ></div>
-     <image class= "image"></image>`)
-
-draw();
- */
