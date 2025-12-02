a4e83807e5f51e8986c6483627cccc52c894a568
jasonz0704 <oonetwome@gmail.com>
2025-12-02 14:02:27 -0500

    7

--- app/src/main.js
+++ app/src/main.js
@@ -3,3 +3,20 @@ import "./style.css";
 document.getElementById("memeform").addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
+const memeform = document.getElementById("memeform");
+const topinput = document.getElementById("toptext");
+const bottominput = document.getElementById("bottomtext");
+const urlinput = document.getElementById("imageurl");
+const memecanvas = document.getElementById("memecanvas");
+const ctx = memecanvas.getContext("2d");
+
+memeform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  generatememe();
+});
+
+function generateMeme() {
+  const toptext = topinput.value;
+  const bottomtext = bottominput.value;
+  const imageurl = urlinput.value;
