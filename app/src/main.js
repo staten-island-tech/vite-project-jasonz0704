7ca0c1799f755eaf7da9c48620616c73e4af928a
jasonz0704 <oonetwome@gmail.com>
2025-12-05 14:02:01 -0500

    9

--- app/src/main.js
+++ app/src/main.js
@@ -1,30 +1,16 @@
 import "./style.css";
 
-//prevent the refresh
-document.getElementById("memeform").addEventListener("submit", function (e) {
-  e.preventDefault();
-});
-
 const memeform = document.getElementById("memeform");
 const topinput = document.getElementById("toptext");
 const bottominput = document.getElementById("bottomtext");
 const urlinput = document.getElementById("imageurl");
 
-//when form submits generate meme
-memeform.addEventListener("submit", (e) => {
-  generatememe();
-});
+//prevent the refresh
 
-function generatememe() {
-  /* const toptext = topinput.value;
-  const bottomtext = bottominput.value;
-  const imageurl = urlinput.value; */
-  const memecanvas = document.querySelector(".memecanvas");
-  memecanvas.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="toptext">${topinput}</div>
-    <img class="image" src="${urlinput}">
-    <div class="bottomtext">${bottominput}</div>`
-  );
-  console.log("memegenerated");
-}
+memeform.getElementById("memeform").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  toptext.textContent = topinput.value;
+  bottomtext.textContent = bottominput.value;
+  memeimage.src = urlinput.value;
+});
