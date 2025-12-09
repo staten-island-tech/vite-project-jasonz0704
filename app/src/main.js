9d6325743c6c4e769b55b1868a837aad5fae0970
jasonz0704 <oonetwome@gmail.com>
2025-12-09 14:02:19 -0500

    11

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,14 @@
 import "./style.css";
 
 const memeform = document.getElementById("memeform");
-const topinput = document.getElementById("toptext");
-const bottominput = document.getElementById("bottomtext");
-const urlinput = document.getElementById("imageurl");
 
+const topinput = document.getElementById("topinput");
 const toptext = document.getElementById("toptext");
+
+const bottominput = document.getElementById("bottominput");
 const bottomtext = document.getElementById("bottomtext");
+
+const urlinput = document.getElementById("urlinput");
 const memeimage = document.getElementById("memeimage");
 
 //prevent the refresh
