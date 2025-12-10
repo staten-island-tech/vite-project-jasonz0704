df9f502398bcb1edbd6a0f8f38c7d5fecaf88b48
jasonz0704 <oonetwome@gmail.com>
2025-12-10 14:01:47 -0500

    12

--- app/src/main.js
+++ app/src/main.js
@@ -20,3 +20,13 @@ memeform.addEventListener("submit", function (e) {
   bottomtext.textContent = bottominput.value;
   memeimage.src = urlinput.value;
 });
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
